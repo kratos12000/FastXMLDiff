3051cf67c2a2b482796e8172fadc8e8c3ea57d28
Dr. Robert Marmorstein <marmorsteinrm@longwood.edu>
2011-04-19 14:12:12 -0400

    Remove unnecessary MainWindow class.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-#include "MainWindow.h"
+#include "DiffWidget.h"
 #include <QApplication>
+#include <QWidget>
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
-	MainWindow *window = new MainWindow;
+	DiffWidget *window = new DiffWidget;
 	window->setWindowTitle("XML Diff");
 	window->show();
+	QObject::connect(window, SIGNAL(canceled()), &app, SLOT(quit()));
 	app.exec();	
 
 }
